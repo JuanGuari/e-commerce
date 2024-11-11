a10f044650df3ed2f1455b6633f74cf5eff83c5f
Juan Guari <developer@openix.com.ar>
2024-11-11 19:40:57 -0300

    feat(frontend): view products list

--- Frontend/src/app/features/products/components/product-list/product-list.component.ts
+++ Frontend/src/app/features/products/components/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +9,53 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
+
+  productos: any[] = [
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 1',
+      precio: 1000
+    },
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 2',
+      precio: 2000
+    },
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 3',
+      precio: 3000
+    },
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 1',
+      precio: 1000
+    },
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 2',
+      precio: 2000
+    },
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 3',
+      precio: 3000
+    },
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 1',
+      precio: 1000
+    },
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 2',
+      precio: 2000
+    },
+    {
+      imagen: 'https://via.placeholder.com/150',
+      nombre: 'Producto 3',
+      precio: 3000
+    },
+  ];
 
 }
