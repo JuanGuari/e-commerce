f37762b1f72c653c7da4aa420d0961ca11c41a72
Juan Guari <developer@openix.com.ar>
2024-11-12 23:43:15 -0300

    feat(backend): unit tests

---
+++ Backend/BackendTest/Tests/ProductControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Core.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebApp.Controllers.v1;
+
+namespace BackendTest.Tests
+{
+    public class ProductControllerTests
+    {
+        private readonly Mock<IProductManager> _mockProductManager;
+        private readonly ProductController _controller;
+
+        public ProductControllerTests()
+        {
+            _mockProductManager = new Mock<IProductManager>();
+            _controller = new ProductController(_mockProductManager.Object);
+        }
+
+        [Fact]
+        public async Task GetProduct_ReturnsOkResult_WhenProductExists()
+        {
+            // Arrange
+            var productDTO = new ProductDTO
+            {
+                Id = 1,
+                Name = "Test Product",
+                Description = "Ropa",
+                Category = "A",
+                Price = 100,
+                ImageUrl = "image_url_here",
+            };
+
+            var resultOperation = new ResultOperation<ProductDTO>
+            {
+                Data = productDTO,
+                IsSuccess = true,
+                Message = "Product found"
+            };
+
+            _mockProductManager.Setup(service => service.GetById(1)).ReturnsAsync(resultOperation);
+
+            // Act
+            var result = await _controller.GetById(1);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedProduct = Assert.IsType<ProductDTO>(okResult.Value);
+            Assert.Equal("Test Product", returnedProduct.Name);
+        }
+    }
+}
---
+++ Backend/BackendTest/Tests/ProductRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendTest.Tests
+{
+    public class ProductRepositoryTest
+    {
+        private readonly DbContextOptions<AppDbContext> _options;
+
+        public ProductRepositoryTest()
+        {
+            _options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+        }
+
+
+        [Fact]
+        public async void AddProduct_ShouldAddProductToDatabase()
+        {
+            // Arrange
+            var context = new AppDbContext(_options);
+            var repository = new ProductRepository(context);
+            var newProduct = new ProductEntity
+            {
+                Id = 1,
+                Name = "Test Product",
+                Description = "Ropa",
+                Category = "A",
+                Price = 100,
+                ImageUrl = "image_url_here",
+            };
+
+            // Act
+            await repository.Insert(newProduct);
+            context.SaveChanges();
+
+            // Assert
+            var product = context.Productos.FirstOrDefault(p => p.Id == 1);
+            product.Should().NotBeNull();
+            product.Name.Should().Be("Test Product");
+        }
+    }
+}
